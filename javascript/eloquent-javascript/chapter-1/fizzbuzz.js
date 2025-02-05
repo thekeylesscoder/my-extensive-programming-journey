01da07c400f17f6408e118ea7fd38060311b0659
thekeylesscoder <50999620+thekeylesscoder@users.noreply.github.com>
2025-02-04 21:27:48 -0500

    First draft of almost functional program

--- javascript/eloquent-javascript/chapter-1/fizzbuzz.js
+++ javascript/eloquent-javascript/chapter-1/fizzbuzz.js
@@ -1,1 +1,15 @@
-console.log();
+let count = 0;
+for (count; count <= 100; count++) {
+  if (count % 3 != 0 && count % 5 != 0) {
+    console.log(count);
+  }
+  else if (count % 3 == 0) {
+  console.log("Fizz");
+  }
+  else if (count % 5 == 0) {
+    console.log("Buzz");
+  }
+ else if (count % 3 == 0 && count % 5 == 0) {
+    console.log("FizzBuzz");
+  }
+}
