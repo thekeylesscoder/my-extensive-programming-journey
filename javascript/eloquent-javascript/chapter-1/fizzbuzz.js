95107106792ab6ceeffa998bdfe6038aa5ce866a
thekeylesscoder <50999620+thekeylesscoder@users.noreply.github.com>
2025-02-05 19:36:35 -0500

    Add instructions

--- javascript/eloquent-javascript/chapter-1/fizzbuzz.js
+++ javascript/eloquent-javascript/chapter-1/fizzbuzz.js
@@ -1,3 +1,12 @@
+/* PROGRAM DEFINITION
+
+Print all numbers between 1 to 100 using console.log except in the following cases:
+- "Fizz" must be printed for numbers divisible by 3
+- "Buzz" must be printed for numbers divisible by 5
+
+Afterwards, modify the program to print "FizzBuzz" for numbers divisible by both 3 and 5.
+*/
+
 let count = 0;
 for (count; count <= 100; count++) {
   if (count % 3 != 0 && count % 5 != 0) {
