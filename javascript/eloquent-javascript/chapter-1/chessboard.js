2cb3e447ae2b95df3f119ef6a0367db2f05789a9
thekeylesscoder <50999620+thekeylesscoder@users.noreply.github.com>
2025-02-05 19:22:51 -0500

    Create chessboard.js and add instructions

---
+++ javascript/eloquent-javascript/chapter-1/chessboard.js
@@ -0,0 +1,15 @@
+/* PROGRAM REQUIREMENTS DEFINED
+
+Program must display 8x8 grid display with newline characters separating lines akin to a chessboard. Spaces must either have a # or a space. Initially create a program with the following output:
+
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # #
+
+Afterwards, make a variable (or binding) size = 8 and modify the display so it works for any size.
+*/ 
