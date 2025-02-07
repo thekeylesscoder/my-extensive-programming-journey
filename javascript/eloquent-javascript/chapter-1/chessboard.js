6cb8c7e09d50e271605e971ee0f0bf4da0b401e9
thekeylesscoder <50999620+thekeylesscoder@users.noreply.github.com>
2025-02-06 20:51:31 -0500

    First attempt - doesn't correctly print out chessboard

--- javascript/eloquent-javascript/chapter-1/chessboard.js
+++ javascript/eloquent-javascript/chapter-1/chessboard.js
@@ -13,3 +13,18 @@ Program must display 8x8 grid display with newline characters separating lines a
 
 Afterwards, make a variable (or binding) size = 8 and modify the display so it works for any size.
 */ 
+
+
+let lineCount = 0;
+let characterCount = 0;
+let size = 8;
+
+while (lineCount < 8) {
+  if (lineCount % 2 == 0) {
+    console.log(" # # # #");
+    lineCount++;
+  } else if (lineCount % 3 == 0) {
+    console.log("# # # # ");
+    lineCount++;
+  }
+}
