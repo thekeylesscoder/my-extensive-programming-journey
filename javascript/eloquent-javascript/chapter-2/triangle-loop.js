7e4fe72abcbcdedb8f84e6eea6f802cb25d1c275
thekeylesscoder <50999620+thekeylesscoder@users.noreply.github.com>
2025-02-23 16:34:00 -0500

    Create triangle-loop.js
    Delete/move from Chapter 1 directory

---
+++ javascript/eloquent-javascript/chapter-2/triangle-loop.js
@@ -0,0 +1,23 @@
+/* PROGRAM INSTRUCTIONS
+
+This program needs to output a triangle with seven different lines of output, as seen below:
+
+#
+##
+###
+####
+#####
+######
+#######
+
+Haverbeke gives a hint to use the .length attribute for any string.
+
+*/
+
+let counter = 0;
+let hashtagCount = "#";
+while (counter <= 7) {
+  counter++;
+  console.log(hashtagCount);
+  console.log("/n");
+}
