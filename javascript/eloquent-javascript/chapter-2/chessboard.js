399543eae1d71392b4e6be5355596024530e4a67
thekeylesscoder <50999620+thekeylesscoder@users.noreply.github.com>
2025-04-12 19:28:48 -0400

    Updated draft code
    Still causes an error but want to look back to see how much progress was made.

--- javascript/eloquent-javascript/chapter-2/chessboard.js
+++ javascript/eloquent-javascript/chapter-2/chessboard.js
@@ -14,6 +14,25 @@ Program must display 8x8 grid display with newline characters separating lines a
 Afterwards, make a variable (or binding) size = 8 and modify the display so it works for any size.
 */ 
 
+/*
+
+Second draft - still causing an error, "RangeError: Potential infinite loop: exceeded 2000 iterations.", but wanted to show progress.
+let size = 0;
+
+while (size <= 8) {
+  if (size % 2 == 0) {
+    console.log(" # # # #");
+    size++;
+  } else if (size % 3 == 0) {
+    console.log("# # # # ");
+    size++;
+  }
+  if (size == 8) {
+    break;
+  }
+}
+*/
+
 
 let lineCount = 0;
 let characterCount = 0;
