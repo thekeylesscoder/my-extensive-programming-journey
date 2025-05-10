289e47b089baf23aa728076fab869d20a48b0307
thekeylesscoder <50999620+thekeylesscoder@users.noreply.github.com>
2025-05-10 12:50:41 -0400

    Update chessboard.js
    Final version

--- javascript/eloquent-javascript/chapter-2/chessboard.js
+++ javascript/eloquent-javascript/chapter-2/chessboard.js
@@ -25,40 +25,15 @@ for (size; size <= 7; size++) {
   else console.log("# # # # ");
 }
 
-// Modified program - prints out one row then stops. Need to further modify.
-
-let size = 8;
-
-for (size; size >= 0; size++) {
- /* This code alone causes the program to potentially get stuck in an infinite loop and doesn't execute anything. 
- Simply reversing the logic causes the same issue.*/ 
- 
-  if (size % 2 == 0) {
-  console.log(" # # # #\n");
-    }
-  else if (size % 2 == 1) {
-    console.log("# # # # ");
-  }
- // The break statement needs additional input which is what causes it to only display one line.
-  break;
-}
-
-// Version 3 - variables for strings. Causing an infinite loop so need to rework this.
+// Final program
 
 let size = 8;
 let lightSpace = " # # # #";
 let darkSpace = "# # # # ";
 
-for (size; size <= 0; size++) {
- if (size % 2 == 0) {
+for (size; size > 0; size--) {
+  if (size % 2 == 0) {
   console.log(lightSpace);
- }
- else if (size % 2 == 1) {
-  console.log(darkSpace);
- }
- else if (size == 0) {
-  break;
- }
+  }
+  else console.log(darkSpace);
 }
-
-
