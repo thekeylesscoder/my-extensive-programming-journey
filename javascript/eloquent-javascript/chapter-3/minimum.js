1373bf376d3e5f1d4526aae73cb3516c3b66d804
thekeylesscoder <50999620+thekeylesscoder@users.noreply.github.com>
2025-05-14 08:45:38 -0400

    Initial minimum number program
    Need to test and refine this program

--- javascript/eloquent-javascript/chapter-3/minimum.js
+++ javascript/eloquent-javascript/chapter-3/minimum.js
@@ -1,7 +1,12 @@
 // program that returns the smallest value between 2 given numbers
-
 function returnMinimum() {
-  // First and second user values; undeclared
+   // First and second user values; undeclared
   let value1;
   let value2;
-};
+  // Need to add restrictions that ensure that only numbers and no other characters are included here
+  console.log("Please input your first number: ");
+  prompt(value1);
+  console.log("Please input your second number: ");
+  prompt(value2);
+  console.log("Between these two numbers, the smallest given to me is " + Math.min(value1, value2));
+}
