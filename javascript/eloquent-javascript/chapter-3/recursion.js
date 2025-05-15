96aa40e8e131b90979be8f0e6d5371b05748d659
thekeylesscoder <50999620+thekeylesscoder@users.noreply.github.com>
2025-05-15 10:19:49 -0400

    First recursion.js draft

--- javascript/eloquent-javascript/chapter-3/recursion.js
+++ javascript/eloquent-javascript/chapter-3/recursion.js
@@ -1,1 +1,11 @@
+// Initial program - needs more functionality and testing
 
+function isEven() {
+  let positiveWholeNumber;
+  let boolean;
+  prompt("Enter a whole number (no negatives): ");
+  
+}
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
