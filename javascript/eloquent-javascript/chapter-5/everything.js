89778bea96b0990691703f86e3759465a7e32bc3
thekeylesscoder <50999620+thekeylesscoder@users.noreply.github.com>
2025-05-25 08:33:13 -0400

    Create everything.js
    Initial notes for exercise

---
+++ javascript/eloquent-javascript/chapter-5/everything.js
@@ -0,0 +1,5 @@
+/* Make two different versions "every" method typically found in arrays as a function. The first should utilize a loop while the other uses the some method. */
+
+// Version 1
+
+// Version 2
