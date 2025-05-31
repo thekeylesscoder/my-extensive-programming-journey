8869eb1ced979f30241cd9d230b2dd409e2cb66a
thekeylesscoder <50999620+thekeylesscoder@users.noreply.github.com>
2025-05-31 11:11:49 -0400

    Create measuring-a-robot.js

---
+++ javascript/eloquent-javascript/chapter-7/measuring-a-robot.js
@@ -0,0 +1,1 @@
+// A function, compareRobots, needs to have two robots, what they have stored, and make 100 jobs that lets each find solutions. The average steps taken by each should be returned at the end.
