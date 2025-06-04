7fabff0f57c3ffd953f4b82a7814a16cdf8dfb97
thekeylesscoder <50999620+thekeylesscoder@users.noreply.github.com>
2025-06-04 12:05:16 -0400

    Create regexp-golf.js

---
+++ javascript/eloquent-javascript/chapter-9/regexp-golf.js
@@ -0,0 +1,11 @@
+/* This regular expression should see if the following substrings:
+- car and cat
+- pop and prop
+- ferret, ferry, and ferrari
+- Words with an "ious" ending
+- Whitespace character with punctuation (period, comma, colon, or semicolon)
+- Six-letter plus words
+- Words without lowercase or capitalized e in strings
+
+See if you can make it as small as possible.
+*/
