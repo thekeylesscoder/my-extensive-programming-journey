dcdbfc529b5ee1ea172a4702c6b127c2f3882740
thekeylesscoder <50999620+thekeylesscoder@users.noreply.github.com>
2025-06-04 12:19:21 -0400

    Update bean-counting.js

--- javascript/eloquent-javascript/chapter-3/bean-counting.js
+++ javascript/eloquent-javascript/chapter-3/bean-counting.js
@@ -1,3 +1,12 @@
-function countBs() {
+/* Write two functions - countBs (takes a string argument and returning a count of the capital Bs in string) as & a variation, countChar, that takes another argument - what character is being found and summed.
+ */
+
+// var string is the only input
+
+function countBs(var string) {
   console.log("Input a string: ");
+}
+
+function countChar(var string, var characterSummed) {
+  console.log("Input a string and the character you want to count: );
 }
