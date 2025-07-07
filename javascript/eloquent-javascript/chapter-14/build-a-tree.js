f296506a14c7ee0007855d4454068171e5f06f91
thekeylesscoder <50999620+thekeylesscoder@users.noreply.github.com>
2025-07-07 12:31:43 -0400

    Create build-a-tree.js

---
+++ javascript/eloquent-javascript/chapter-14/build-a-tree.js
@@ -0,0 +1,9 @@
+// Make a program that takes the values from a data set, mountains, that makes a DOM structure that gives a column and row for each object and header rows. Columns should be automatically returning property names, and add an element with an id "mountains" so it's displayed and right-align specific values.
+
+<!DOCTYPE html>
+<table id="mountains">
+  
+</>
+</html>
+
+
