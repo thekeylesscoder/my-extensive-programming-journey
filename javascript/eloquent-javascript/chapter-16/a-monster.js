3e40bd802a2dbfc377f58c43e940bca81c694188
thekeylesscoder <50999620+thekeylesscoder@users.noreply.github.com>
2025-07-29 08:02:19 -0400

    Create a-monster.js

---
+++ javascript/eloquent-javascript/chapter-16/a-monster.js
@@ -0,0 +1,6 @@
+/* Add an item, which the exercise refers to as a monster, that meets the following requirements:
+
+ - It only moves horizontally.
+ - You can choose the direction the monster moves in towards the player, i.e. moving in the players' direction; back and forth; etc. It doesn't require falling but shouldn't be able to walk through walls. 
+ - Once the player is touched by the monster, the player should either make the monster disappear (if the monster's bottom is under the top of the player) or end the game. 
+*/
