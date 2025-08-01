7671a483631386c7d07be3cff4077ff25e64325a
thekeylesscoder <50999620+thekeylesscoder@users.noreply.github.com>
2025-08-01 18:22:18 -0400

    Create a-bouncing-ball.js

---
+++ javascript/eloquent-javascript/chapter-17/a-bouncing-ball.js
@@ -0,0 +1,1 @@
+// Draw a box with a bouncing ball that moves at the same speed and bounces off the sides when it hits the sides with the requestAnimationFrame technique (shown in Chapters 14 and 16).
