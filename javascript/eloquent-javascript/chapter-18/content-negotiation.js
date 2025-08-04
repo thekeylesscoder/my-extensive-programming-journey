3d7aff61a9b274b3b82a8b66a7aeb7b9787a392a
thekeylesscoder <50999620+thekeylesscoder@users.noreply.github.com>
2025-08-04 08:04:32 -0400

    Create content-negotiation.js

---
+++ javascript/eloquent-javascript/chapter-18/content-negotiation.js
@@ -0,0 +1,7 @@
+/* HTTP can perform content negotiation, which can tell the header the document type the client wants to receive via Accept. 
+Complete the following tasks
+- Send requests to get the following media types from https://eloquentjavascript.net/author - text/plain, text/html, and application/json. 
+- Set the headers property to Accept for each media type. 
+- Request and see what status code is sent back after requesting application/rainbows+unicorns.
+*/
+
