c64a588f9e18abb0c042b47b852b37b15ca72950
thekeylesscoder <50999620+thekeylesscoder@users.noreply.github.com>
2025-08-11 20:27:05 -0400

    Create keyboard-bindings.js

---
+++ javascript/eloquent-javascript/chapter-19/keyboard-bindings.js
@@ -0,0 +1,4 @@
+/* Include shortcuts for keyboard keys to the web application, where the tool's first letter selects it, and CTRL or CMD of the same letter undos it.
+This can be accomplished with the PixelEditor component, where the tabIndex 0 property can get focused by the keyboard. Consider key event handlers
+with this exercise, and that events associated with the keyboard (ctrlKey and metaKey) can check if keys are being held.
+*/
