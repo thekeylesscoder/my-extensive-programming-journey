b72529b69de5ae5cbdcc6c20425f6968c66ed2d5
thekeylesscoder <50999620+thekeylesscoder@users.noreply.github.com>
2025-08-18 10:57:43 -0400

    Create search-tool.js

---
+++ javascript/eloquent-javascript/chapter-20/search-tool.js
@@ -0,0 +1,3 @@
+/* Create a Node script running from command line that is similar to the Unix command line tool grep that can search files for regular espressions. It should use the first command line argument as a regular expression, while other arguments should be files to look for, and output names of files where the content matches regular expression. Once this functions, add on to the functionality so that when a directory is an argument, the search goes through every file in its directory and subdirectories.
+Asynchronous or synchronous files system functions should be used if needed.
+*/
