90a99cfac4724ebe715b00e1df6de369398cbacc
thekeylesscoder <50999620+thekeylesscoder@users.noreply.github.com>
2025-08-26 10:35:52 -0400

    Update triangle-loop.js
    Remove alternate version

--- javascript/eloquent-javascript/chapter-2/triangle-loop.js
+++ javascript/eloquent-javascript/chapter-2/triangle-loop.js
@@ -14,21 +14,6 @@ Haverbeke gives a hint to use the .length attribute for any string.
 
 */
 
-/*
-  Potentially using this logic to print out the correct # - need to elaborate and test.
-  
-  let counter = 0;
-let hashtagCount = "#";
-while (counter < 7) {
-  counter++;
-  if (counter.length % 2 == 0) {
-    console.log("#/n");
-  }
-  else if (counter.length % 2 == 1) {
-    console.log("##/n");
-  }
-*/
-
 let counter = 0;
 let hashtagCount = "#";
 while (counter < 7) {
